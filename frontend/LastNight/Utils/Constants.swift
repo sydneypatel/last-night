bc2bf37c24b97c9c922c9bfc0022aefbf6631692
Sydney <sydney.j.patel@gmail.com>
2026-07-28 12:12:03 -0400

    updating to elastic IP instance

--- frontend/LastNight/Utils/Constants.swift
+++ frontend/LastNight/Utils/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Constants {
-    static let apiBaseURL = "http://18.218.17.79:3000"
+    static let apiBaseURL = "http://16.58.164.94:3000"
     static let s3BucketName = "last-night-photos"
     static let awsRegion = "us-east-2"
     static let cdnBaseURL = "https://d3cqszhzen72wj.cloudfront.net"
