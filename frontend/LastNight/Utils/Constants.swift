e4e6a901bf4d2ee061de8cd54a0b74903e3baf23
Sydney <sydney.j.patel@gmail.com>
2026-07-28 13:02:58 -0400

    update again

--- frontend/LastNight/Utils/Constants.swift
+++ frontend/LastNight/Utils/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Constants {
-    static let apiBaseURL = "http://16.58.164.94:3000"
+    static let apiBaseURL = "http://3.137.201.245:3000"
     static let s3BucketName = "last-night-photos"
     static let awsRegion = "us-east-2"
     static let cdnBaseURL = "https://d3cqszhzen72wj.cloudfront.net"
